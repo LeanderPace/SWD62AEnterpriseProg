817a805cedfe78170bef6451172552d3d0c122f7
LeanderPace <leander.pace.b42329@mcast.edu.mt>
2020-10-27 21:49:33 +0100

    3rd Commit

--- SWD62AEnterpriseProg/Presentation/Controllers/SupportController.cs
+++ SWD62AEnterpriseProg/Presentation/Controllers/SupportController.cs
@@ -8,8 +8,28 @@ namespace Presentation.Controllers
 {
     public class SupportController : Controller
     {
+
+        // this method will be called using a get request and its purpose is to load the page
+        [HttpGet]
         public IActionResult Contact()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Contact(string email, string query)
+        {
+            if (string.IsNullOrEmpty(query))
+            {
+                ViewData["warning"] = "Question was left empty";
+            }
+            else
+            {
+                ViewData["feedback"] = "Thank you for your query, we will get back to you asap";
+            }
+
+            //ViewBag.feedback = "";
+            
             return View();
         }
     }
