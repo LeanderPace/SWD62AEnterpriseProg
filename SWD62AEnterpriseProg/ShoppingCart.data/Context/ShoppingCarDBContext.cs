05ad3733b37bac236562cc3801a98aa32742b91c
LeanderPace <leander.pace.b42329@mcast.edu.mt>
2020-10-30 09:47:55 +0100

    Database Connection

---
+++ SWD62AEnterpriseProg/ShoppingCart.data/Context/ShoppingCarDBContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShoppingCart.data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppingCart.data.Context
+{
+    public class ShoppingCarDBContext : DbContext
+    {
+        public ShoppingCarDBContext(DbContextOptions options)
+            : base(options)
+        { }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Product>().Property(x => x.Id).HasDefaultValueSql("NEWID()");
+        }
+    }
+}
---
+++ SWD62AEnterpriseProg/ShoppingCart.domain/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Data.SqlClient.DataClassification;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ShoppingCart.data.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+    }
+}
