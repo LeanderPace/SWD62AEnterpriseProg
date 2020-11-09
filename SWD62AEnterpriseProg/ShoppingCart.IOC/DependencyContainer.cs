2ceac651d4a1e803b195bfa09039541b907c04c6
Leander Pace <leander.pace.b42329@mcast.edu.mt>
2020-11-09 09:20:40 +0100

    Dependency Injection (latest)

---
+++ SWD62AEnterpriseProg/ShoppingCart.IOC/DependencyContainer.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using ShoppingCart.application.Interfaces;
+using ShoppingCart.application.Services;
+using ShoppingCart.data.Repositories;
+using ShoppingCart.domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppingCart.IOC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            // when are these instances triggered?
+            // answer: https://www.tutorialsteacher.com/core/dependency-injection-in-aspnet-core
+
+            services.AddScoped<iProductsRepository, ProductsRepository>();
+            services.AddScoped<iProductsService, ProductsService>();
+        }
+    }
+}
