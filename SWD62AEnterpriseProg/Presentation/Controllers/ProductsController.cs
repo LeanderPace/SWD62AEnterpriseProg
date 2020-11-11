3cf7d43956b562978b6e434443a188d6a5b2d4c5
LeanderPace <leander.pace.b42329@mcast.edu.mt>
2020-11-11 13:43:49 +0100

    presenting our products list on a view

---
+++ SWD62AEnterpriseProg/Presentation/Controllers/ProductsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ShoppingCart.application.Interfaces;
+using ShoppingCart.application.Services;
+
+namespace Presentation.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private iProductsService _productsService;
+        public ProductsController(iProductsService productsService)
+        {
+            _productsService = productsService;
+        }
+
+        public IActionResult Index()
+        {
+            // IQueryable
+            // IEnumerable
+            // List >>>> IEnumerable >>> IQueryable
+
+            var list = _productsService.GetProducts();
+
+            return View(list);
+        }
+    }
+}
--- SWD62AEnterpriseProg/ShoppingCart.IOC/DependencyContainer.cs
+++ SWD62AEnterpriseProg/ShoppingCart.IOC/DependencyContainer.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using ShoppingCart.application.Interfaces;
 using ShoppingCart.application.Services;
+using ShoppingCart.data.Context;
 using ShoppingCart.data.Repositories;
 using ShoppingCart.domain.Interfaces;
 using System;
@@ -11,10 +13,15 @@ namespace ShoppingCart.IOC
 {
     public class DependencyContainer
     {
-        public static void RegisterServices(IServiceCollection services)
+        public static void RegisterServices(IServiceCollection services, string connectionString)
         {
             // when are these instances triggered?
             // answer: https://www.tutorialsteacher.com/core/dependency-injection-in-aspnet-core
+
+
+            services.AddDbContext<ShoppingCarDBContext>(options =>
+                options.UseSqlServer(connectionString
+                    ));
 
             services.AddScoped<iProductsRepository, ProductsRepository>();
             services.AddScoped<iProductsService, ProductsService>();
